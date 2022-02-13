a69636079d7091c889193d9346ff280bb1d2da0e
Matheusrrcs <matheus-santana-msn@hotmail.com>
2022-02-13 20:15:49 -0300

    Ligações telas do lol

---
+++ assets/js/telas.js
@@ -0,0 +1,29 @@
+let value 
+
+document.querySelectorAll(".nav-link").forEach( function(button) {
+    
+    button.addEventListener("click", function(event) {
+    const el = event.target;
+    value = el.innerText;
+   
+  });
+  
+});
+
+
+function modalChange2(){
+    
+    if(value == "MD10"){
+        window.location.href ='md10_lol.html';
+       
+    }
+    else if(value.trim() == "ELO BOOST"){
+        window.location.href ='Eloboost_lol.html';
+    }
+    else if(value == "DUO BOOST"){
+        window.location.href ='DuosBoost_lol.html'
+    }
+   else{
+    window.location.href ='coach_lol.html'
+   }
+ }
