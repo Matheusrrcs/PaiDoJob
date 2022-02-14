fb437c059d81e7f6c72e066b05fe8ffbbeb651b1
Matheusrrcs <matheus-santana-msn@hotmail.com>
2022-02-13 23:40:05 -0300

    colocado os elos

---
+++ assets/js/chagenImgELoRifit.js
@@ -0,0 +1,102 @@
+
+function changeImg() {
+    let slc = document.getElementById("ligaatual").value
+   
+    let igm = document.getElementById("emblem")
+    let div = document.getElementById("boxDivisaoAtual")
+   
+
+    if (slc == "ferro") {
+        igm.src = "imagens/Emblem_Iron.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "bronze") {
+        igm.src = "imagens/Emblem_Bronze.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "prata") {
+        igm.src = "imagens/Emblem_Silver.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "ouro") {
+        igm.src = "imagens/Emblem_Gold.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "platina") {
+        igm.src = "imagens/Emblem_Platinum.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "esmeralda") {
+        igm.src = "imagens/Wild_Rift_Emerald_rank.webp"
+        div.classList.remove("invisible")
+    }
+    if (slc == "diamante") {
+        igm.src = "imagens/Emblem_Diamond.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "mestre") {
+        igm.src = "imagens/Emblem_Master.png"
+        div.classList.add("invisible")
+    }
+    if (slc == "grao-mestre") {
+        igm.src = "imagens/Emblem_Grandmaster.png"
+        div.classList.add("invisible")
+    }
+    if (slc == "desafiante") {
+        igm.src = "imagens/Emblem_Challenger.png"
+       div.classList.add("invisible")
+    }
+    
+}
+
+
+function changeImg2() {
+    let slc = document.getElementById("ligadesejada").value
+   
+    let igm = document.getElementById("emblem2")
+    console.log(igm)
+    let div = document.getElementById("boxDivisaoDesejada")
+   
+
+    if (slc == "ferro") {
+        igm.src = "imagens/Emblem_Iron.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "bronze") {
+        igm.src = "imagens/Emblem_Bronze.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "prata") {
+        igm.src = "imagens/Emblem_Silver.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "ouro") {
+        igm.src = "imagens/Emblem_Gold.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "platina") {
+        igm.src = "imagens/Emblem_Platinum.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "esmeralda") {
+        igm.src = "imagens/Wild_Rift_Emerald_rank.webp"
+        div.classList.remove("invisible")
+    }
+    if (slc == "diamante") {
+        igm.src = "imagens/Emblem_Diamond.png"
+        div.classList.remove("invisible")
+    }
+    if (slc == "mestre") {
+        igm.src = "imagens/Emblem_Master.png"
+        div.classList.add("invisible")
+    }
+    if (slc == "grao-mestre") {
+        igm.src = "imagens/Emblem_Grandmaster.png"
+        div.classList.add("invisible")
+    }
+    if (slc == "desafiante") {
+        igm.src = "imagens/Emblem_Challenger.png"
+       div.classList.add("invisible")
+    }
+    
+}
---
+++ assets/js/changeImgMd10.js
@@ -0,0 +1,98 @@
+
+function changeImg() {
+    let slc = document.getElementById("ligaatual").value
+   
+    let igm = document.getElementById("emblem")
+  
+    if (slc == "sem-ranking") {
+        igm.src = "imagens/Emblem_none.png"
+       
+    }
+    if (slc == "ferro") {
+        igm.src = "imagens/Emblem_Iron.png"
+       
+    }
+    if (slc == "bronze") {
+        igm.src = "imagens/Emblem_Bronze.png"
+       
+    }
+    if (slc == "prata") {
+        igm.src = "imagens/Emblem_Silver.png"
+        
+    }
+    if (slc == "ouro") {
+        igm.src = "imagens/Emblem_Gold.png"
+       
+    }
+    if (slc == "platina") {
+        igm.src = "imagens/Emblem_Platinum.png"
+        
+    }
+    if (slc == "diamante") {
+        igm.src = "imagens/Emblem_Diamond.png"
+       
+    }
+    if (slc == "mestre") {
+        igm.src = "imagens/Emblem_Master.png"
+        
+    }
+    if (slc == "grao-mestre") {
+        igm.src = "imagens/Emblem_Grandmaster.png"
+       
+    }
+    if (slc == "desafiante") {
+        igm.src = "imagens/Emblem_Challenger.png"
+       
+    }
+    
+}
+
+
+function changeImg2() {
+    let slc = document.getElementById("ligaatual2").value
+   
+    let igm = document.getElementById("emblem2")
+ 
+
+    if (slc == "ferro") {
+        igm.src = "imagens/Emblem_Iron.png"
+       
+    }
+    if (slc == "bronze") {
+        igm.src = "imagens/Emblem_Bronze.png"
+      
+    }
+    if (slc == "prata") {
+        igm.src = "imagens/Emblem_Silver.png"
+    
+    }
+    if (slc == "ouro") {
+        igm.src = "imagens/Emblem_Gold.png"
+        
+    }
+    if (slc == "platina") {
+        igm.src = "imagens/Emblem_Platinum.png"
+        
+    }
+    if (slc == "esmeralda") {
+        igm.src = "imagens/Wild_Rift_Emerald_rank.webp"
+        
+    }
+    if (slc == "diamante") {
+        igm.src = "imagens/Emblem_Diamond.png"
+         
+    }
+    if (slc == "mestre") {
+        igm.src = "imagens/Emblem_Master.png"
+        
+    }
+    if (slc == "grao-mestre") {
+        igm.src = "imagens/Emblem_Grandmaster.png"
+         
+    }
+    if (slc == "desafiante") {
+        igm.src = "imagens/Emblem_Challenger.png"
+       
+    }
+    
+}
