028f18d95e040a509babd9446cedb27fbbe5e7ea
Matheusrrcs <matheus-santana-msn@hotmail.com>
2022-02-13 21:52:35 -0300

    telas do wild rift e comunicação

--- assets/js/telas.js
+++ assets/js/telas.js
@@ -5,14 +5,14 @@ document.querySelectorAll(".nav-link").forEach( function(button) {
     button.addEventListener("click", function(event) {
     const el = event.target;
     value = el.innerText;
-   
+  
   });
   
 });
 
 
 function modalChange2(){
-    
+    console.log(value)
     if(value == "MD10"){
         window.location.href ='md10_lol.html';
        
@@ -23,7 +23,24 @@ function modalChange2(){
     else if(value == "DUO BOOST"){
         window.location.href ='DuosBoost_lol.html'
     }
-   else{
+   else if(value=="COACH"){
     window.location.href ='coach_lol.html'
+   }
+ }
+
+ 
+function modalChange3(){
+    console.log(value)
+    if(value == "MD10"){
+        window.location.href ='md10_rift.html';
+    }
+    else if(value.trim() == "ELO BOOST"){
+        window.location.href ='Eloboost_rift.html';
+    }
+    else if(value == "DUO BOOST"){
+        window.location.href ='DuosBoost_rift.html'
+    }
+   else if(value=="COACH"){
+    window.location.href ='coach_rift.html'
    }
  }
