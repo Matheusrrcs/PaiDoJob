b570f2b99776aaf2979fd6214c5f445837177059
Matheusrrcs <matheus-santana-msn@hotmail.com>
2022-03-01 00:49:00 -0300

    neto pediu

--- js/cahangePriceCoach.js
+++ js/cahangePriceCoach.js
@@ -9,7 +9,7 @@ const coachPrice = {
 const desconto = 20
 const descontoP = 10
 
-document.getElementById("valor").innerHTML = `<small >POR:</small>  ${moedaBrasil(  coachPrice.cinco)} </p>`
+document.getElementById("valor").innerHTML = `<small >POR:</small>  ${moedaBrasil((1 - descontoP / 100) * coachPrice.cinco)} </p>`
 
 // document.getElementById("valorAntigo").innerHTML = `<small >de:</small>  ${moedaBrasil((desconto / 100 + 1) * coachPrice.cinco)}</p>`
 
@@ -27,8 +27,13 @@ function Eloprice() {
     descontoFinal = (desconto / 100 + 1) * resultadoFinal
     
 
+<<<<<<< HEAD
     document.getElementById("valor").innerHTML = `<small style="font-size: 20px;">POR:</small> ${moedaBrasil(resultadoFinal)}</p>`
     // document.getElementById("valorAntigo").innerHTML = `<small >de:</small>  ${moedaBrasil(descontoFinal)}</p>`
+=======
+    document.getElementById("valor").innerHTML = `<small style="font-size: 20px;">POR:</small> R$ ${ descontoPincipal},00</p>`
+    document.getElementById("valorAntigo").innerHTML = `<small >de:</small>  ${moedaBrasil(descontoFinal)}</p>`
+>>>>>>> parent of 4cca3af (desconto tirado)
 
 }
 
