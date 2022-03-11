cce840d202d5233d2d16b598feff1aceeffedab1
Matheus Rosario <79647787+Matheusrrcs@users.noreply.github.com>
2022-03-11 18:18:17 -0300

    colocado o desconto do pai30

--- js/changeImgMd10.js
+++ js/changeImgMd10.js
@@ -9,4 +9,4 @@ function changeImg() {
     
 }
 document.getElementById("valorAntigo").innerHTML =' <small style="font-size: 20px;">DE:</small> R$ 90,00'
-document.getElementById("valor").innerHTML =' <small style="font-size: 20px;">POR:</small> R$ 75,00'
+document.getElementById("valor").innerHTML =' <small style="font-size: 20px;">POR:</small> R$ 67,50'
